737b50858336b989fd09d42770df959b89dd16c3
nverno <noah.v.peart@gmail.com>
2020-12-22 11:09:08 -0800

    init repo

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
