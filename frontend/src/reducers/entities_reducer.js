11f81623949e619fbcf6138b06035f84e5eadd78
Kevin Bastoul <kaycbas@gmail.com>
2020-12-29 13:08:45 -0800

    update projects reducer

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import users from './entities/users_reducer';
+import projects from './entities/projects_reducer';
 
 export default combineReducers({
   users,
+  projects,
 });
