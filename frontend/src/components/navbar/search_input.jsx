85ca7581d651cbdd48a1aa8083b3e37a8e4316ab
nverno <noah.v.peart@gmail.com>
2020-12-31 22:54:13 -0800

    Clear tags when route changes

--- frontend/src/components/navbar/search_input.jsx
+++ frontend/src/components/navbar/search_input.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import InputBase from '@material-ui/core/InputBase';
-
 import { connect } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+
 import { setSearch } from '../../actions/search_actions';
 import { fetchProjects } from '../../actions/project_actions';
+import { clearTags } from '../../actions/tag_actions';
 
 const mapStateToProps = (state, _ownProps) => {
   const { search, tags } = state.ui;
@@ -13,11 +15,25 @@ const mapStateToProps = (state, _ownProps) => {
 const mapDispatchToProps = (dispatch) => ({
   setSearch: (searchString) => dispatch(setSearch(searchString)),
   fetchProjects: (filter) => dispatch(fetchProjects(filter)),
+  clearTags: () => dispatch(clearTags()),
 });
 
 // Debounce reference:
 // https://dev.to/jasonnordheim/debounce-performance-and-react-4de1
-const SearchInput = ({ search, tags, setSearch, fetchProjects, ...props }) => {
+const SearchInput = ({
+  search,
+  tags,
+  setSearch,
+  clearTags,
+  fetchProjects,
+  ...props
+}) => {
+  // clear tags when route changes
+  let location = useLocation();
+  useEffect(() => {
+    clearTags();
+  }, [location]);
+
   // debouncing .3 seconds
   useEffect(() => {
     const timeout = setTimeout(() => {
