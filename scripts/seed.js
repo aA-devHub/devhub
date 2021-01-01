eabfb1d80eb5f764ec927046c7832a84611c3c52
nverno <noah.v.peart@gmail.com>
2021-01-01 15:39:47 -0800

    don't fail if no seed data

--- scripts/seed.js
+++ scripts/seed.js
@@ -3,11 +3,9 @@
 
 const mongoose = require('mongoose');
 const db = require('../config/keys').mongoURI;
+const path = require('path');
 
 const Message = require('../models/Message');
-const messages = require('../data/messagedata.json');
-
-console.dir(messages);
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -15,13 +13,22 @@ mongoose
     console.log('Connected to MongoDB successfully');
 
     // Messages
-    let res = await Message.deleteMany({ createdAt: null });
-    if (res.ok === 1) {
-      console.log('Removed messages with no timestamp');
-    }
+    try {
+      const messages = require('../data/messagedata.json');
+      console.dir(messages);
 
-    const msgs = messages.map((msg) => new Message(msg));
-    res = await Message.collection.insertMany(msgs);
+      // Cleanup any bad data
+      let res = await Message.deleteMany({ createdAt: null });
+      if (res.ok === 1) {
+        console.log('Removed messages with no timestamp');
+      }
+
+      const msgs = messages.map((msg) => new Message(msg));
+      res = await Message.collection.insertMany(msgs);
+    } catch (err) {
+      // console.error(err.message);
+      console.log('No message data... skipping seeding messages');
+    }
 
     mongoose.connection.close();
   })
