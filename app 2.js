8ece4520f5154d88150c8c52171e9d95b6d6024e
Kevin Bastoul <kaycbas@gmail.com>
2021-01-01 16:47:47 -0800

    remove all duplicates ... wtf

--- app 2.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
