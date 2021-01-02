ded642d01f3a6cac968dd30d7f0e9748b933be77
nverno <noah.v.peart@gmail.com>
2021-01-02 03:09:33 -0800

    populate name after reading conversation

--- routes/api/conversations.js
+++ routes/api/conversations.js
@@ -5,6 +5,9 @@ const passport = require('passport');
 const Conversation = require('../../models/Conversation');
 // const validateConversation = require('../../validation/conversations');
 
+// TODO: add filtered search
+// https://stackoverflow.com/questions/11303294/querying-after-populate-in-mongoose
+// needs to filter by participant name after populate, see $lookup
 const buildSearchFilter = ({ search }) => {
   return search
     ? {
@@ -21,8 +24,6 @@ router.get(
   async (req, res) => {
     const user = req.user;
 
-    // TODO: add filtered search
-    // https://stackoverflow.com/questions/11303294/querying-after-populate-in-mongoose
     const filter = buildSearchFilter(req.query);
 
     let conversations = await Conversation.find({ participants: user._id })
@@ -53,16 +54,17 @@ router.get(
           select: 'name',
         },
       })
+      .populate('participants', 'name')
+      .populate('unreadBy', 'name')
       .then((conversation) => {
         const messages = conversation.messages.slice();
         conversation.messages = messages.map((e) => e._id);
 
         // Mark conversation as read by current user if not already
-        const idx = conversation.unreadBy.indexOf(userId);
-        if (idx !== -1) {
-          conversation.unreadBy.splice(idx, 1);
-          conversation.save();
-        }
+        conversation.unreadBy = conversation.unreadBy.filter(
+          (x) => !x._id.equals(userId)
+        );
+        conversation.save();
 
         res.json({ conversation, messages });
         // XXX: remove this after notifications are updated
