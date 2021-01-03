9320746186e7ec2de43043d38ffd1b77a0829e4e
Kevin Bastoul <kaycbas@gmail.com>
2021-01-02 21:59:45 -0800

    fix message notifications refresh

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -105,7 +105,10 @@ function Navbar(props) {
 
   let location = useLocation();
   useEffect(() => {
-    props.fetchNotifications();
+    // props.fetchNotifications();
+    setTimeout(() => {
+      props.fetchNotifications();
+    }, 300);
   }, [location]);
 
   const handleProfileMenuOpen = (event) => {
