69d68395c35321514350320627eaea198cff5437
Kevin Bastoul <kaycbas@gmail.com>
2021-01-02 21:44:04 -0800

    delete duplicates, wtf 2

--- frontend/src/util/notification_api_util 2.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const fetchNotifications = () => {
-  return axios.get(`/api/notifications`);
-};
-
-export const removeProjectNotification = (projectId) => {
-  return axios.delete(`/api/notifications/projects/${projectId}`);
-};
