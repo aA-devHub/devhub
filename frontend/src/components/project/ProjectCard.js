eb573d4e7372ea0263de8afd71bbc6c562bacdc0
nverno <noah.v.peart@gmail.com>
2021-01-03 00:50:11 -0800

    hookup backend to project card/s

--- frontend/src/components/project/ProjectCard.js
+++ frontend/src/components/project/ProjectCard.js
@@ -1,83 +1,83 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+
 import Carousel from 'react-material-ui-carousel';
 import { useHistory } from 'react-router-dom';
 import { makeStyles, Avatar, Typography } from '@material-ui/core';
 import { Star, KeyboardArrowUp } from '@material-ui/icons';
-import * as COLORS from '../../colors';
 
-// function ProjectCard({ proj }) {
-function ProjectCard({ project }) {
-  const history = useHistory();
-  const proj = {
-    user: {
-      id: 1,
-      name: 'Jesse Alba',
-      yearsOfExperience: 10,
-      title: 'frontend',
-      avatarUrl:
-        'https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzJ8fGF2YXRhcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
-    },
-    images: [
-      'https://images.unsplash.com/photo-1541882270037-d3cf216b2ca4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80',
-      'https://images.unsplash.com/photo-1541848952518-f6c52dbc7c94?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1951&q=80',
-      'https://images.unsplash.com/photo-1480350376518-4575ee35bf49?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80',
-    ],
-    techsUsed: ['ruby', 'react', 'mern'],
-    peopleFavorited: [1, 2, 3, 4],
-    title: 'a title',
+// import { project as dummy } from './__tests__/dummy_data';
+import * as COLORS from '../../colors';
+import { getImageArray } from '../../selectors/projects';
+
+const mapStateToProps = (state, { project }) => {
+  const user = state.entities.users[project.user];
+  // XXX: is this supposed to be different than imageUrl?
+  user.avatarUrl = user.imageUrl;
+
+  return {
+    user,
   };
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexDirection: 'column',
-      padding: '1rem',
-    },
-    image: {
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '1rem',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    marginBottom: '.3rem',
+    cursor: 'pointer',
+    '& > img': {
       width: '100%',
       height: '100%',
       borderRadius: 10,
-      marginBottom: '.3rem',
-      cursor: 'pointer',
-      '& > img': {
-        width: '100%',
-        height: '100%',
-        borderRadius: 10,
-      },
     },
-    userDisplay: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      padding: '0 10px',
-    },
-    leftPanel: {
-      display: 'flex',
-      alignItems: 'center',
-      cursor: 'pointer',
-    },
-    avatar: {
-      width: '3rem',
-      height: '3rem',
-      marginRight: 10,
-    },
-    userInfo: {
-      display: 'flex',
-      alignItems: 'center',
-    },
-    title: {
-      backgroundColor: COLORS.DEVDARKBLUE,
-      color: 'white',
-      padding: '0 7px',
-      marginLeft: 10,
-      alignText: 'center',
-      borderRadius: 7,
-    },
-  }));
+  },
+  userDisplay: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '0 10px',
+  },
+  leftPanel: {
+    display: 'flex',
+    alignItems: 'center',
+    cursor: 'pointer',
+  },
+  avatar: {
+    width: '3rem',
+    height: '3rem',
+    marginRight: 10,
+  },
+  userInfo: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  title: {
+    backgroundColor: COLORS.DEVDARKBLUE,
+    color: 'white',
+    padding: '0 7px',
+    marginLeft: 10,
+    alignText: 'center',
+    borderRadius: 7,
+  },
+}));
+
+function ProjectCard({ project, user }) {
+  const history = useHistory();
+
   const [autoplay, setAutoplay] = useState(false);
   const classes = useStyles();
+
   const favoriteProject = () => {
     // favorite();
   };
+
   return (
     <div className={classes.root}>
       <Carousel
@@ -88,13 +88,13 @@ function ProjectCard({ project }) {
         indicators={false}
         timeout={500}
       >
-        {proj.images.map((item, i) => (
+        {getImageArray(project).map((item, i) => (
           <div
             className={classes.image}
             key={i}
             onMouseOver={() => setAutoplay(true)}
             onMouseOut={() => setAutoplay(false)}
-            onClick={() => history.push('/projects/5fea1ae674646d29c7ddb2ee')}
+            onClick={() => history.push(`/projects/${project._id}`)}
           >
             <img src={item} alt={item.title} />
           </div>
@@ -103,7 +103,7 @@ function ProjectCard({ project }) {
 
       <div className={classes.userDisplay}>
         <div className={classes.leftPanel}>
-          <Avatar className={classes.avatar} src={proj.user.avatarUrl} />
+          <Avatar className={classes.avatar} src={user.avatarUrl} />
           <div>
             <div className={classes.userInfo}>
               <Typography
@@ -113,19 +113,17 @@ function ProjectCard({ project }) {
                   fontWeight: 800,
                 }}
               >
-                {proj.user.name}
+                {user.name}
               </Typography>
               <div className={classes.title}>
-                <Typography style={{ fontSize: 13 }}>
-                  {proj.user.title}
-                </Typography>
+                <Typography style={{ fontSize: 13 }}>{user.title}</Typography>
               </div>
             </div>
             <div>
               <Typography
                 style={{ color: 'red', fontSize: 15, fontWeight: 500 }}
               >
-                {proj.user.yearsOfExperience} years of experience
+                {user.yearsOfExperience} years of experience
               </Typography>
             </div>
           </div>
@@ -142,4 +140,4 @@ function ProjectCard({ project }) {
   );
 }
 
-export default ProjectCard;
+export default connect(mapStateToProps)(ProjectCard);
--- frontend/src/components/project/ProjectCards.js
+++ frontend/src/components/project/ProjectCards.js
@@ -1,51 +1,73 @@
-import Card from './ProjectCard';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core';
 
-function ProjectCards({ projects }) {
+import Card from './ProjectCard';
+import { fetchProjects } from '../../actions/project_actions';
+
+const mapStateToProps = (state, _ownProps) => {
+  const { search, tags } = state.ui;
+  const { projects } = state.entities;
+  return {
+    search,
+    tags,
+    projects,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchProjects: (filter) => dispatch(fetchProjects(filter)),
+});
+
+function ProjectCards({ projects, fetchProjects, search, tags }) {
   const useStyles = makeStyles((theme) => ({
     root: {
       padding: '20px 5%',
     },
   }));
   const classes = useStyles();
+
+  useEffect(() => {
+    fetchProjects({ search, tags });
+  }, []);
+
   return (
     <Grid container className={classes.root}>
-      {/* {projects.map((project) => (
-        <Grid item md={3} sm={6}>
-          <Card />
+      {Object.values(projects).map((project, idx) => (
+        <Grid item md={3} sm={6} key={idx}>
+          <Card project={project} />
         </Grid>
-      ))} */}
-      <Grid item md={4} sm={6}>
-        <Card />
-      </Grid>
-      <Grid item md={4} sm={6}>
-        <Card />
-      </Grid>
-      <Grid item md={4} sm={6}>
-        <Card />
-      </Grid>
-      <Grid item md={4} sm={6}>
-        <Card />
-      </Grid>
-      <Grid item md={4} sm={6}>
-        <Card />
-      </Grid>
-      <Grid item md={4} sm={6}>
-        <Card />
-      </Grid>
-      <Grid item md={4} sm={6}>
-        <Card />
-      </Grid>
-      <Grid item md={4} sm={6}>
-        <Card />
-      </Grid>
-      <Grid item md={4} sm={6}>
-        <Card />
-      </Grid>
+      ))}
+      {/*   <Grid item md={4} sm={6}> */}
+      {/*     <Card /> */}
+      {/*   </Grid> */}
+      {/*   <Grid item md={4} sm={6}> */}
+      {/*     <Card /> */}
+      {/*   </Grid> */}
+      {/*   <Grid item md={4} sm={6}> */}
+      {/*     <Card /> */}
+      {/*   </Grid> */}
+      {/*   <Grid item md={4} sm={6}> */}
+      {/*     <Card /> */}
+      {/*   </Grid> */}
+      {/*   <Grid item md={4} sm={6}> */}
+      {/*     <Card /> */}
+      {/*   </Grid> */}
+      {/*   <Grid item md={4} sm={6}> */}
+      {/*     <Card /> */}
+      {/*   </Grid> */}
+      {/*   <Grid item md={4} sm={6}> */}
+      {/*     <Card /> */}
+      {/*   </Grid> */}
+      {/*   <Grid item md={4} sm={6}> */}
+      {/*     <Card /> */}
+      {/*   </Grid> */}
+      {/*   <Grid item md={4} sm={6}> */}
+      {/*     <Card /> */}
+      {/*   </Grid> */}
     </Grid>
   );
 }
 
-export default ProjectCards;
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectCards);
