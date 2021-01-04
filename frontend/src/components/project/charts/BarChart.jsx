5607c91bbd5b843eeb6e9a72fe35a19d6fd0931b
nverno <noah.v.peart@gmail.com>
2021-01-03 20:55:51 -0800

    Make pie chart prettier

--- frontend/src/components/project/charts/BarChart.jsx
+++ frontend/src/components/project/charts/BarChart.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
 
+const renderLabel = ({ x, y, name, fill, midAngle, ...props }) => {
+  console.log('Props: ', props);
+  const anchor = midAngle > 45 && midAngle < 275 ? 'end' : 'start';
+  return (
+    <text
+      x={x}
+      y={y}
+      fill={fill}
+      width={100}
+      height={100}
+      fontSize={30}
+      textAnchor={anchor}
+    >
+      {name}
+    </text>
+  );
+};
+
 const BarChart = ({ project }) => (
   <div style={{ width: '50%', height: '50%' }}>
     <img src="https://res.cloudinary.com/willwang/image/upload/v1609703195/barchart_se1ck8.png" />
--- frontend/src/components/project/charts/PieChart.jsx
+++ frontend/src/components/project/charts/PieChart.jsx
@@ -1,29 +1,95 @@
-import { PieChart, Pie, Cell } from 'recharts';
+import React from 'react';
+// import { Typography } from '@material-ui/core';
+import {
+  // ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell,
+  // Legend, Tooltip
+} from 'recharts';
 
 const RADIAN = Math.PI / 180;
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884d8'];
 
-const TechPieChart = ({ data, ...props }) => (
-  <div>
-    <div style={{ width: '50%', height: '50%' }}>
+const renderLabel = ({ x, y, name, fill, midAngle, ...props }) => {
+  console.log('Props: ', props);
+  const anchor = midAngle > 45 && midAngle < 275 ? 'end' : 'start';
+  return (
+    <text
+      x={x}
+      y={y}
+      fill={fill}
+      width={100}
+      height={100}
+      fontSize={30}
+      textAnchor={anchor}
+    >
+      {name}
+    </text>
+  );
+};
+
+// const RenderTooltip = ({ active, payload, label }) => {
+//   console.log('Label: ', label);
+//   if (active) {
+//     return (
+//       <div className="language-tooltip">
+//         <Typography>{label}</Typography>
+//       </div>
+//     );
+//   }
+//   return null;
+// };
+
+// const CustomTooltip = ({ active, payload, label }) => {
+//   if (active) {
+// 	return (
+// 	  <div className="custom-tooltip">
+// 		<p className="label">{`${label} : ${payload[0].value}`}</p>
+// 		{/* <p className="intro">{getIntroOfPage(label)}</p> */}
+// 		<p className="desc">Anything you want can be displayed here.</p>
+// 	  </div>
+// 	);
+//   }
+//   return null;
+// };
+
+const TechPieChart = ({ data, ...props }) => {
+  // const [activeIndex, setActiveIndex] = React.useState(0);
+  // const onPieEnter = (data, index) => {
+  //   setActiveIndex(index);
+  // };
+  return (
+    // <ResponsiveContainer>
+    <div style={{ width: '100%', height: '100%' }}>
       <PieChart {...props}>
         <Pie
           data={data}
-          cx={120}
-          cy={200}
-          innerRadius={60}
-          outerRadius={80}
+          /* cx={120} */
+          /* cy={200} */
+          innerRadius={120}
+          outerRadius={180}
           fill="#8884d8"
           paddingAngle={5}
+          /* onMouseEnter={onPieEnter} */
+          label={renderLabel}
         >
           {data.map((entry, index) => (
             <Cell fill={COLORS[index % COLORS.length]} />
           ))}
+          {/* <Legend /> */}
+          {/* <Tooltip */}
+          {/*   position={{ y: -50 }} */}
+          {/*   cursor={true} */}
+          {/*   content={<CustomTooltip />} */}
+          {/* /> */}
+          {/* <Tooltip content={renderTooltip}/> */}
         </Pie>
       </PieChart>
-      {/* <img src="https:res.cloudinary.com/willwang/image/upload/v1609703195/barchart_se1ck8.png" /> */}
     </div>
-  </div>
-);
+    // </ResponsiveContainer>
+  );
+};
+
 export default TechPieChart;
