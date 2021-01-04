a2c208a7716a7e82a5b9dc829479995635cbe383
nverno <noah.v.peart@gmail.com>
2021-01-04 01:04:06 -0800

    final

--- frontend/src/selectors/projects.js
+++ frontend/src/selectors/projects.js
@@ -6,7 +6,7 @@ export const getImageArray = (project) => {
 
 export const byDate = (a, b) =>
   Date.parse(b.createdAt) - Date.parse(a.createdAt);
-export const byPopularity = (a, b) => a.numFavorites - b.numFavorites;
+export const byPopularity = (a, b) => b.numFavorites - a.numFavorites;
 export const sortProjects = (projects, sorter) => {
   if (!projects || !Object.values(projects).length) return null;
   let res = Object.values(projects);
