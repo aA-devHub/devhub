317f1f4b0d9a75c702ff76954f88df9cf7542940
Yizhe Wang <yizhe87@gmail.com>
2021-01-20 17:56:04 -0800

    footer cursor changed

--- frontend/src/components/main/Footer.js
+++ frontend/src/components/main/Footer.js
@@ -129,7 +129,12 @@ const Footer = () => {
         >
           <Avatar
             src="https://res.cloudinary.com/willwang/image/upload/v1609184704/logoround_tgdkrs.png"
-            style={{ width: 100, height: 100, opacity: '.9' }}
+            style={{
+              width: 100,
+              height: 100,
+              opacity: '.9',
+              cursor: 'pointer',
+            }}
             onClick={() => history.push('/')}
           />
         </div>
